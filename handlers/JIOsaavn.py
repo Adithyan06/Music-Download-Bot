421515230ed176165ddcaf71fc556524a6d398a6
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2022-06-22 18:54:53 +0530

    Delete JIOsaavn.py

--- handlers/JIOsaavn.py
+++
@@ -1,9 +0,0 @@
-import aiohttp
-import requests
-
-JIO = "http://starkmusic.herokuapp.com/result/?query={}"
-
-async def JIO(query: str):
-    async with requests.get(url=JIO(query)) as res:
-        return (await res.json())["results"] if ((await res.json()).get("results", None) is not None) else []
-
