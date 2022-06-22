008dd19edd410d15a597243c8bf825941fee9ebb
Peter Parker <105187918+Adithyan06@users.noreply.github.com>
2022-06-22 18:46:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,5 +9,3 @@ AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "NACBots")
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
-ARQ_API_KEY = getenv("ARQ_API_KEY")
-ARQ_API_URL = "https://grambuilders.tech/"
